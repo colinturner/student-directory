7ab979cfb9f3049f1a7f660bcf6e2aae93fb2158
Colin Turner <colinturner92@gmail.com>
2017-03-01 17:38:07 +0000

    Updated Ex 8-12

--- Ex_8/Ex_8-12.rb
+++ Ex_8/Ex_8-12.rb
@@ -1,48 +1,75 @@
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
+  puts "Or press return again to finish"
+  # We create an empty array
   students = []
-  # get the first name
-  name = $stdin.gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-  # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-  # get another name from the user
-    name = $stdin.gets.chomp
+
+  puts "The name, please:"
+  name = gets.delete("\n")
+
+# Until the name variable is empty, repeat this code
+  until name.empty? do
+  # Add the student hash to the array
+    puts "The cohort, please:"
+    cohort = gets.chomp
+      if cohort.empty?
+        cohort = :november
+      else
+        cohort = cohort.downcase.to_sym
+      end
+
+    students << {name: name, cohort: cohort}
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
     end
-  # return the array of students
+  # Get another name from the user
+    puts "The next name, please (or press RETURN again to finish):"
+    name = gets.chomp
+
+  end
+  # Once the while loop has exited, return the array of students
   students
 end
 
 
-def print_header(students)
-  if students.count > 0
-    puts "The students of my cohort at Makers Academy"
-    puts "-------------"
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
+end
+
+
+
+def print(students)  # Pass in the students array, which is the returned value from input_students
+  cohorts = students.map {|student| student[:cohort]}   # Produce a new array, containing each cohort value from each hash in the students array
+    cohorts.uniq.each do |month|     # Ignore duplicate values for the months collected into the cohorts array in the line above
+      puts "#{month} cohort"    # puts those unique (non-duplicated) months, along with the word 'cohort' next to the month
+      students.each do |student|   # for each hash in the students array,
+        if student[:cohort] == month  # if the value stored under the :cohort key is precisely equal to the month
+          puts "#{student[:name]}"   # then puts the student's name value
+        end
+      end
+    end
+end
+
+
+
+def print_footer(names)
+  if names.count < 1
+    puts "Whoops! Looks like we have no students! :("
+  elsif names.count == 1
+    puts "Overall, we have #{names.count} great student"
   else
-    puts "Whoops! Looks like we have no students :("
+    puts "Overall, we have #{names.count} great students"
   end
 end
 
 
-def print(students)
-  students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
 
-
-def print_footer(students)
-  if students.count > 0
-    puts "Overall, we have #{students.count} great students"
-  end
-end
-
-
+# Now we have to actually call the methods to make anything happen
 students = input_students
-print_header(students)
+print_header
 print(students)
 print_footer(students)
